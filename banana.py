02f4e3621c96c07db169fb1b887199760fcd61b8
newbie(papu) <bpapu602@gmail.com>
2024-05-13 12:30:29 +0530

    new

--- banana.py
+++ banana.py
@@ -1,7 +1,1 @@
 print("hello world")
-for i in range(2):
-    print("hello world")
-print("are you sure this was the right thing to do?")
-print("this is a staged thing.")
-print("hello world")
-print("correction")
